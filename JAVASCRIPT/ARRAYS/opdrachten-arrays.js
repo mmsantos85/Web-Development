c6c2f8dc6fc98430c1a396ba75f60c14bda0a2be
Melisa <mmsantos85@gmail.com>
2021-01-13 16:47:36 +0100

    JAVASCRIPT tussen push.

---
+++ JAVASCRIPT/ARRAYS/opdrachten-arrays.js
@@ -0,0 +1,18 @@
+//Opdracht 1: Maak een array met 10 getallen die willekeurig in een array staan. Print de array uit in jouw console.-->  
+const randomNumbers = [7, 3, 6, 4, 2, 9, 1, 0, 8, 5]; //[] hiermee geef je een array aan.
+console.log(randomNumbers);
+
+//Opdracht 2: Maak een array met 5 verschillende fruitsoorten. Print de array uit in jouw console.
+const fruitsoorten = ["Mango", "Aardbei", "Appel", "Sinaasappel", "Passievrucht"];
+console.log(fruitsoorten);
+
+fruit1 = fruitsoorten[2]; // vraag 3.
+fruit2 = fruitsoorten[1]; // vraag 4.
+console.log(fruitsoorten[1]); // Uitvoer is Aardbei.
+
+// Opdracht 3: Op welke index staat “Appel”. Gebruik de index waarde om “Appel” in jouw console uit te printen.
+// Opdracht 4:Op welke index staat “Aardbei”. Gebruik de index waarde om “Aardbei” in jouw console uit teprinten.
+
+// Opdracht 5;
+
+
