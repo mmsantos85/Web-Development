41aa684bc48fefde3e18cff46c1e5202aefc5c68
Melisa <mmsantos85@gmail.com>
2021-01-28 20:23:06 +0100

    JAVASCRIPT eindopdracht tussen push.

--- JAVASCRIPT/EIND-OPDRACHT/script.js
+++ JAVASCRIPT/EIND-OPDRACHT/script.js
@@ -3,39 +3,80 @@ const display = document.getElementById("display");
 const clearButton = document.getElementById("clearButton");
 const plusMinus= document.getElementById("plusMinus");
 const decimal = document.getElementById("decimal");
-const operator = document.getElementById("operator");
+const operators = document.getElementById("operator");
+
 // selectors classes:
 const displayOutput = document.getElementsByClassName("display-output");
 const buttonNumber = document.getElementsByClassName("button-number");
+
+// alleen bij = uitkomsten gebruiken.
 let one = "";
 let two = "";
 let numbersList = [];
+
+
+function screen() {
+    return document.getElementById("display").innerHTML;   
+}
+
 
 function clear () {
     display.innerHTML = "0";
     numbersList = [];
     clearButton.innerText = "AC";
-    console.log("hello")
+    //console.log("hello")
 }
 
+
+function operatorplus(nummerInput) {
+    const plus = document.getElementById("+").innerHTML;
+    document.getElementById("display").innerHTML = screen() + plus;
+    numbersList.push(nummerInput.innerHTML);
+    //console.log(plus);
+}
+
+function operatorminus(nummerInput) {
+    const min = document.getElementById("minus").innerHTML;
+    document.getElementById("display").innerHTML = screen() + min; 
+    numbersList.push(nummerInput.innerHTML); 
+}
+
+function operatordiv(nummerInput) {
+    const delen = document.getElementById("division").innerHTML;
+    document.getElementById("display").innerHTML = screen() + delen;
+    numbersList.push(nummerInput.innerHTML);
+}
+
+function operatormultiply(nummerInput) {
+    const keer = document.getElementById("multiply").innerHTML;
+    document.getElementById("display").innerHTML = screen() + keer;
+    numbersList.push(nummerInput.innerHTML);
+}
+
+function operatorresultaat() {
+    const resultaat = document.getElementById("display").value;
+    resultaat = eval(resultaat);
+    document.getElementById("display").innerHTML = resultaat; 
+    // numbersList.push(nummerInput.innerHTML);
+    // display.innerText = numbersList.join("");
+    // numbersList = [];
+    // console.log(resultaat);
+}
+
+
+// Push functie, voegt nieuwe element toe aan het einde van de array.
+//The join() method returns the array as a string.
 function nummerInvoer(nummerInput) {    
-numbersList.push(nummerInput.innerHTML);
-display.innerText = numbersList.join("");
-clearButton.innerText = "C";
-
+    numbersList.push(nummerInput.innerHTML);
+    display.innerText = numbersList.join("");
+    clearButton.innerText = "C";
+    console.log(nummerInput);
 }
-
-
-
-
-
-
-
 
 
 
 clearButton.addEventListener("click", clear);
-// van de element voegen wij een even listener toe "click"= click event 
-//en na de comma  pak je de functienaam. Eventlistener staat altijd aan het einde van je js bestand.
+// van de element voegen wij een event listener toe "click"= click event 
+//en na de comma  pak je de functienaam. Eventlistener staat altijd aan het einde van je JS bestand.
 
 
