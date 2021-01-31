548e779a0e1de50953b19b07ee413de95024e002
Melisa <mmsantos85@gmail.com>
2021-01-31 16:33:54 +0100

    JAVASCRIPT eindopdracht,  tussen push.

--- JAVASCRIPT/EIND-OPDRACHT/script.js
+++ JAVASCRIPT/EIND-OPDRACHT/script.js
@@ -14,12 +14,12 @@ let one = "";
 let two = "";
 let numbersList = [];
 
-
+// Display aanroepen.
 function screen() {
     return document.getElementById("display").innerHTML;   
 }
 
-
+//Function clear button.
 function clear () {
     display.innerHTML = "0";
     numbersList = [];
@@ -27,42 +27,62 @@ function clear () {
     //console.log("hello")
 }
 
+// Kleurverandering oranje naar wit en weer terug naar oranje. Gebruik gemaakt van querySElector.
+document.querySelectorAll(".button-orange").forEach(function(obj) {
+    obj.addEventListener("mousedown", function () {
+        this.style.backgroundColor = "white";
+        this.style.color = "orange";
+    })
+    obj.addEventListener("mouseup", function () {
+        this.style.backgroundColor = "orange";
+        this.style.color = "white";   
+    })
+});
 
+// Functions basis berekeningen:
 function operatorplus(nummerInput) {
     const plus = document.getElementById("+").innerHTML;
-    document.getElementById("display").innerHTML = screen() + plus;
-    numbersList.push(nummerInput.innerHTML);
-    //console.log(plus);
+    document.getElementById("display").innerHTML = screen() + plus; 
+    numbersList.push(nummerInput.innerHTML); 
 }
-
+    
 function operatorminus(nummerInput) {
     const min = document.getElementById("minus").innerHTML;
     document.getElementById("display").innerHTML = screen() + min; 
     numbersList.push(nummerInput.innerHTML); 
 }
 
-function operatordiv(nummerInput) {
+function operatordivide(nummerInput) {
     const delen = document.getElementById("division").innerHTML;
     document.getElementById("display").innerHTML = screen() + delen;
     numbersList.push(nummerInput.innerHTML);
 }
 
 function operatormultiply(nummerInput) {
-    const keer = document.getElementById("multiply").innerHTML;
-    document.getElementById("display").innerHTML = screen() + keer;
+    const vermenigvuldig = document.getElementById("multiply").innerHTML;
+    document.getElementById("display").innerHTML = screen() + vermenigvuldig;
     numbersList.push(nummerInput.innerHTML);
 }
-
-function operatorresultaat() {
-    const resultaat = document.getElementById("display").value;
-    resultaat = eval(resultaat);
-    document.getElementById("display").innerHTML = resultaat; 
-    // numbersList.push(nummerInput.innerHTML);
-    // display.innerText = numbersList.join("");
-    // numbersList = [];
-    // console.log(resultaat);
+function operatormodulos(nummerInput) {
+    const modulo = document.getElementById("%").innerHTML;
+    document.getElementById("display").innerHTML = screen() + modulo;
+    numbersList.push(nummerInput.innerHTML);
+    console.log(modulo);
 }
 
+function operatorPlusMin(nummerInput) {
+    const plusMin = document.getElementById("plusMinus").innerHTML;
+    document.getElementById("display").innerHTML = screen() + plusMin;
+    numbersList.push(nummerInput.innerHTML);
+    console.log();
+}
+
+// function resultaat:
+function operatorresultaat() {
+    let resultaat = document.getElementById("display").innerHTML;
+    resultaat = eval(resultaat);
+    document.getElementById("display").innerHTML = resultaat; 
+}
 
 // Push functie, voegt nieuwe element toe aan het einde van de array.
 //The join() method returns the array as a string.
@@ -70,13 +90,12 @@ function nummerInvoer(nummerInput) {
     numbersList.push(nummerInput.innerHTML);
     display.innerText = numbersList.join("");
     clearButton.innerText = "C";
-    console.log(nummerInput);
+    //console.log(nummerInput);
 }
 
 
-
 clearButton.addEventListener("click", clear);
-// van de element voegen wij een event listener toe "click"= click event 
-//en na de comma  pak je de functienaam. Eventlistener staat altijd aan het einde van je JS bestand.
+// van de element voegen wij een event listener toe "click"= click event,
+//na de comma pak je de functienaam. Eventlistener plaats je altijd aan het eind van je JS bestand.
 
 
